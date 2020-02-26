664611ed5dbdcd081b4cb1b3989aacc2f5f4c4d6
nik <nik_mak@bk.ru>
2020-02-26 16:27:01 +0300

    автообновление и убирание кнопки ЕЩЕ

--- src/App.js
+++ src/App.js
@@ -8,28 +8,40 @@ class App extends Component {
         this.state = {
             portion: 1
         };
-
+        this.lastEvents = [null];
         this.incrementPortion = this.incrementPortion.bind(this);
     }
 
     incrementPortion(event) {
         this.setState((state) => {
-            return {portion: state.portion+1}
+            return {portion: state.portion + 1}
         });
         event.preventDefault();
     }
 
     render() {
         let eventsLoaders = [];
-        for(let i = 0; i< this.state.portion; i++){
+        for (let i = 0; i < this.state.portion; i++) {
             eventsLoaders.push(
-                getEventsList((data) => {
+                getEventsList((data, reload) => {
+                    this.lastEvents[i] = data;
+                    setTimeout(reload, 1000);
                     return (
-                        data.map((event) =>
-                            <Event event={event} key={event.id}/>
-                        )
+                        <>
+                            {
+                                data.map((event) => {
+                                    return (
+                                        <Event event={event} key={event.id}/>
+                                    )
+                                })
+                            }
+                            {i === this.state.portion-1 && data.length >= PORTION_SIZE ?
+                                <button onClick={this.incrementPortion} className={"button"}>Еще</button>
+                                : ""
+                            }
+                        </>
                     )
-                }, {limit: PORTION_SIZE, offset: i * PORTION_SIZE}, i)
+                }, {limit: PORTION_SIZE, offset: i * PORTION_SIZE}, i, this.lastEvents)
             );
         }
         return (
@@ -39,9 +51,8 @@ class App extends Component {
                     {
                         eventsLoaders
                     }
-                    <button onClick={this.incrementPortion} className={"button"}>Еще</button>
                 </div>
-                </div>
+            </div>
         )
     }
 }
--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,24 +1,28 @@
 import {Post} from 'react-axios'
 import React from "react";
+
 const SERVER = "http://127.0.0.1:9000";
 
-export const PORTION_SIZE = 10;
+export const PORTION_SIZE = 5;
 
-export const getEventsList = (render, params = {}, key) =>
+export const getEventsList = (render, params = {}, key, lastData) =>
     <Post key={key} url={SERVER + "/api/event/get"} data={params}>
         {(error, response, isLoading, makeRequest, axios) => {
-            if(error) {
+            if (error) {
                 return (<div className={"error-container"}>Что-то пошло не так: {error.message}<br/>
-                <button className={"button"} onClick={() => makeRequest({ params: { reload: true } })}>
-                    Попробовать снова
-                </button></div>)
-            }
-            else if(isLoading) {
-                return (<div>Загрузка...</div>)
-            }
-            else if(response !== null) {
+                    <button className={"button"} onClick={() => makeRequest({params: {reload: true}})}>
+                        Попробовать снова
+                    </button>
+                </div>)
+            } else if (isLoading) {
+                if (lastData[key]) {
+                    return render(lastData[key], makeRequest);
+                } else {
+                    return (<div>Загрузка...</div>)
+                }
+            } else if (response !== null) {
                 return (
-                    render(response.data)
+                    render(response.data, makeRequest)
                 );
             }
             return ("")
